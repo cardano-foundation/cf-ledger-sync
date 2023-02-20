43f09aafc75406e9166a9daf79b5ff3f95464b61
jaspcf <124155250+jaspcf@users.noreply.github.com>
2023-02-20 22:35:23 +0100

    First commit
    Base framework + some testcases for cardano shelly wallet + HTML report integration

---
+++ src/main/java/org/cardanofoundation/utils/RequestResponseFilter.java
@@ -0,0 +1,37 @@
+package org.cardanofoundation.utils;
+
+import io.qameta.allure.Allure;
+import io.restassured.filter.Filter;
+import io.restassured.filter.FilterContext;
+import io.restassured.response.Response;
+import io.restassured.specification.FilterableRequestSpecification;
+import io.restassured.specification.FilterableResponseSpecification;
+
+import java.util.logging.LogRecord;
+
+public class RequestResponseFilter implements Filter {
+
+
+    @Override
+    public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
+        Response response = ctx.next(requestSpec, responseSpec);
+
+        Allure.addAttachment("Request Details: ",
+                "HTTP method: " + requestSpec.getMethod() + "\n" +
+                        "URI: " + requestSpec.getURI() + "\n" +
+                        "Headers:\n" + requestSpec.getHeaders() + "\n" +
+                        "Payload (if applicable): " + requestSpec.getBody() + "\n" +
+                        "Path params (if applicable): " + requestSpec.getPathParams() + "\n" +
+                        "Query params (if applicable): " + requestSpec.getQueryParams() + "\n" +
+                        "Form params (if applicable): " + requestSpec.getFormParams());
+
+        Allure.addAttachment("Response Details: ",
+                "Status code: " + response.getStatusCode() + "\n" +
+                        "Status line: " + response.statusLine() + "\n" +
+                        "Response time: " + response.time() + "\n" +
+                        "Response headers: " + response.headers() + "\n" +
+                        "Response json:\n" + response.getBody().asPrettyString());
+
+        return response;
+    }
+}
