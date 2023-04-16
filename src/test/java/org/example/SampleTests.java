ddc00dfeb328e6dd2d5862833bd5e2cf11e8f20e
jaspcf <124155250+Jassbaavi@users.noreply.github.com>
2023-04-16 20:56:25 +0200

    Update SampleTests.java

--- src/test/java/org/example/SampleTests.java
+++ src/test/java/org/example/SampleTests.java
@@ -30,11 +30,11 @@ public class SampleTests {
 
         IOSDriver iosDriver = appiumServer.launchApp();
 
-        By.ByXPath asds = new By.ByXPath("//XCUIElementTypeTextField[@name=\"IntegerA\"]");
-
-        WebElement element = iosDriver.findElement(asds);
-        element.sendKeys("123");
-        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) iosDriver).getScreenshotAs(OutputType.BYTES)));
+//        By.ByXPath asds = new By.ByXPath("//XCUIElementTypeTextField[@name=\"IntegerA\"]");
+//
+//        WebElement element = iosDriver.findElement(asds);
+//        element.sendKeys("123");
+//        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) iosDriver).getScreenshotAs(OutputType.BYTES)));
 
         sleep(2000);
     }
