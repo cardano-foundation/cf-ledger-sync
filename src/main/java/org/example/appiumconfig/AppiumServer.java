8edd98a423d72ded6e371c57a9bd99abb4c2baa2
jaspcf <124155250+Jassbaavi@users.noreply.github.com>
2023-04-16 20:18:49 +0200

    testing

---
+++ src/main/java/org/example/appiumconfig/AppiumServer.java
@@ -0,0 +1,58 @@
+package org.example.appiumconfig;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ServerSocket;
+import java.net.URL;
+
+public class AppiumServer {
+    private final AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder();
+    private AppiumDriverLocalService server;
+    private int port;
+
+    public AppiumServer() {
+        this.port = getAvailablePort();
+        this.serviceBuilder.usingPort(port);
+        this.server = AppiumDriverLocalService.buildService(serviceBuilder);
+        this.server.start();
+    }
+
+    public AppiumDriverLocalService get(){
+        return this.server;
+    }
+
+    public int getAvailablePort() {
+        int port = 4723;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(0);
+            port = serverSocket.getLocalPort();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return port;
+    }
+
+    public IOSDriver launchApp() throws InterruptedException, MalformedURLException {
+
+        AppiumServer appiumServer = new AppiumServer();
+        AppiumDriverLocalService as = appiumServer.get();
+
+        XCUITestOptions xcuiTestOptions = new XCUITestOptions();
+        xcuiTestOptions.setPlatformName("iOS")
+                .setAutomationName("xcuitest")
+                .setApp("/Users/jaspreetkaur/GitHub/cf-qa-testsuite/src/main/resources/TestApp.app.zip");
+//        IOSDriver iosDriver = new IOSDriver(new URL("http://127.0.0.1:4723/"), xcuiTestOptions);
+        IOSDriver iosDriver = new IOSDriver(as, xcuiTestOptions);
+
+        return iosDriver;
+//        AppiumDriver appiumDriver = new AppiumDriver();
+    }
+}
---
+++ src/test/java/org/example/SampleTests.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import org.example.appiumconfig.AppiumServer;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+
+import java.io.ByteArrayInputStream;
+import java.net.MalformedURLException;
+
+import static java.lang.Thread.sleep;
+
+public class SampleTests {
+
+    @Test
+    public void setup() throws InterruptedException, MalformedURLException {
+
+        extracted();
+        System.out.printf("asdkalsdk");
+    }
+
+
+    @Step("Take Screenshot")
+    private void extracted() throws InterruptedException, MalformedURLException {
+        AppiumServer appiumServer = new AppiumServer();
+        IOSDriver iosDriver = appiumServer.launchApp();
+
+//        By.ByXPath asds = new By.ByXPath("//XCUIElementTypeTextField[@name=\"IntegerA\"]");
+//
+//        WebElement element = iosDriver.findElement(asds);
+//        element.sendKeys("123");
+//        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) iosDriver).getScreenshotAs(OutputType.BYTES)));
+
+        sleep(2000);
+    }
+}
