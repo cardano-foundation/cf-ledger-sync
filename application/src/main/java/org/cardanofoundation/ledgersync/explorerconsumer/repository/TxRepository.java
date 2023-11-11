e01d02eee95c4cef6e63e1a16553fea366bbbaf2
Sotatek-HuyLe3a <huy.le3a@sotatek.com>
2023-11-11 23:13:17 +0700

    feat: change logic when handling tx chart

--- application/src/main/java/org/cardanofoundation/ledgersync/explorerconsumer/repository/TxRepository.java
+++ application/src/main/java/org/cardanofoundation/ledgersync/explorerconsumer/repository/TxRepository.java
@@ -23,12 +23,11 @@ public interface TxRepository extends JpaRepository<Tx, Long> {
 
     @Query("SELECT tx.id as txId, "
             + "tx.block.time as txTime, "
-            + "(SUM(CASE WHEN txo.addressHasScript = TRUE THEN 1 ELSE 0 END) != 0) AS txWithSc, "
-            + "(SUM(CASE WHEN txo.addressHasScript = FALSE AND tm IS NOT NULL THEN 0 ELSE 1 END) = 0) AS txWithMetadataWithoutSc, "
-            + "(SUM(CASE WHEN txo.addressHasScript = TRUE OR tm IS NOT NULL THEN 1 ELSE 0 END) = 0) AS simpleTx "
+            + "(SUM(CASE WHEN r IS NOT NULL THEN 1 ELSE 0 END) != 0) AS txWithSc, "
+            + "(SUM(CASE WHEN r IS NULL AND tm IS NOT NULL THEN 1 ELSE 0 END) != 0) AS txWithMetadataWithoutSc, "
+            + "(SUM(CASE WHEN r IS NULL AND tm IS NULL THEN 1 ELSE 0 END) != 0) AS simpleTx "
             + "FROM Tx tx "
-            + "LEFT JOIN TxIn ti ON ti.txInput = tx "
-            + "LEFT JOIN TxOut txo ON (txo.tx = tx) OR (ti.txOut = txo.tx AND ti.txOutIndex = txo.index and txo.addressHasScript = TRUE) "
+            + "LEFT JOIN Redeemer r on r.tx = tx "
             + "LEFT JOIN TxMetadata tm ON tm.tx = tx "
             + "WHERE :id IS NULL OR tx.id > :id "
             + "GROUP BY txId, txTime")
@@ -36,12 +35,11 @@ public interface TxRepository extends JpaRepository<Tx, Long> {
 
     @Query("SELECT tx.id as txId, "
             + "tx.block.time as txTime, "
-            + "(SUM(CASE WHEN txo.addressHasScript = TRUE THEN 1 ELSE 0 END) != 0) AS txWithSc, "
-            + "(SUM(CASE WHEN txo.addressHasScript = FALSE AND tm IS NOT NULL THEN 0 ELSE 1 END) = 0) AS txWithMetadataWithoutSc, "
-            + "(SUM(CASE WHEN txo.addressHasScript = TRUE OR tm IS NOT NULL THEN 1 ELSE 0 END) = 0) AS simpleTx "
+            + "(SUM(CASE WHEN r IS NOT NULL THEN 1 ELSE 0 END) != 0) AS txWithSc, "
+            + "(SUM(CASE WHEN r IS NULL AND tm IS NOT NULL THEN 1 ELSE 0 END) != 0) AS txWithMetadataWithoutSc, "
+            + "(SUM(CASE WHEN r IS NULL AND tm IS NULL THEN 1 ELSE 0 END) != 0) AS simpleTx "
             + "FROM Tx tx "
-            + "LEFT JOIN TxIn ti ON ti.txInput = tx "
-            + "LEFT JOIN TxOut txo ON (txo.tx = tx) OR (ti.txOut = txo.tx AND ti.txOutIndex = txo.index and txo.addressHasScript = TRUE) "
+            + "LEFT JOIN Redeemer r on r.tx = tx "
             + "LEFT JOIN TxMetadata tm ON tm.tx = tx "
             + "WHERE tx IN (:txs) "
             + "GROUP BY txId, txTime")
