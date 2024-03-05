8c9b43f98bac21fc21ce40840e89b6c93f04019a
Sotatek-QuanLeA <quan.lea@sotatek.com>
2024-03-05 16:06:29 +0700

    feat(verifier-data-app): config multi datasource

---
+++ verifier-data-app/src/test/java/org/cardanofoundation/ledgersync/verifier/data/app/VerifierDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cardanofoundation.ledgersync.verifier.data.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifierDataAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
