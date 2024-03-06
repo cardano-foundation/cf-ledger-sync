6a113378944187df80388484807c2d80478797ba
Satya <satran004@gmail.com>
2024-03-06 13:01:58 +0800

    chore: Add balance views

--- aggregates/account/src/main/resources/db/account/V2_1_1__init.sql
+++ aggregates/account/src/main/resources/db/account/V2_1_1__init.sql
@@ -5,7 +5,7 @@ create table address_tx_amount
     unit               varchar(255),
     tx_hash            varchar(64),
     slot               bigint,
-    quantity           numeric(38)  null,
+    quantity           numeric(38) null,
     addr_full          text,
     policy             varchar(56),
     asset_name         varchar(255),
@@ -18,3 +18,21 @@ create table address_tx_amount
     update_datetime    timestamp,
     primary key (address, unit, tx_hash)
 );
+
+-- address_balance_view
+drop view if exists address_balance_view;
+create view address_balance_view AS
+select ab.*
+from address_balance ab
+         inner join (select address, MAX(slot) as max_slot
+                     from address_balance ab2
+                     group by address) max_ab on ab.address = max_ab.address and ab.slot = max_ab.max_slot;
+
+-- stake_address_balance_view
+drop view if exists stake_address_balance_view;
+create view stake_address_balance_view AS
+select sb.*
+from stake_address_balance sb
+         inner join (select address, MAX(slot) as max_slot
+                     from stake_address_balance sb2
+                     group by address) max_sb on sb.address = max_sb.address and sb.slot = max_sb.max_slot;
