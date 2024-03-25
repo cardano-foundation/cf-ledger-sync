c251057a4cec79832a8b6bbb2c5c992158d58e2a
Sotatek-HuyLe3a <huy.le3a@sotatek.com>
2024-03-25 18:57:34 +0700

    chore: (healthcheck) update message description

--- components/healthcheck/src/main/java/org/cardanofoundation/ledgersync/healthcheck/model/Message.java
+++ components/healthcheck/src/main/java/org/cardanofoundation/ledgersync/healthcheck/model/Message.java
@@ -4,10 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum Message {
-    IS_GOOD("IS_GOOD", "The last time received a block event that is within the threshold"),
-    IS_BAD("IS_BAD", "The last time received a block event that is beyond the threshold"),
+    IS_GOOD("IS_GOOD", "The last time received a block event is within the threshold"),
+    IS_BAD("IS_BAD", "The last time received a block event is beyond the threshold"),
     STOP_SLOT_HAS_REACHED("STOP_SLOT_HAS_REACHED", "Stop slot has been reached"),
-    BLOCK_HAS_REACHED_TIP("BLOCK_HAS_REACHED_TIP", "Block is the tip or near the tip");
+    BLOCK_HAS_REACHED_TIP("BLOCK_HAS_REACHED_TIP", "The latest block is the tip or near the tip");
 
     private final String code;
     private final String desc;
