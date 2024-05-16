781f6f0651249e4b917713506dfdc5efcda4370e
Sotatek-HuyLe3a <huy.le3a@sotatek.com>
2024-05-16 16:04:31 +0700

    chore: rename columns

--- application/src/main/resources/db/migration/ledgersync/V1_1_7__add-missing-columns.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE epoch_param
-    ADD COLUMN dvt_p_p_technical_group double precision,
-    ADD COLUMN dvt_p_p_gov_group double precision,
-    ADD COLUMN dvt_treasury_withdrawal double precision;
-
-ALTER TABLE param_proposal
-    ADD COLUMN dvt_p_p_technical_group double precision,
-    ADD COLUMN dvt_p_p_gov_group double precision,
-    ADD COLUMN dvt_treasury_withdrawal double precision;
