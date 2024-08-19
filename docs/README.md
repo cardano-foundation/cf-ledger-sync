c8287f7876bbdbe0222033a90e530d21a1a0adba
Satya <35016438+satran004@users.noreply.github.com>
2024-08-19 13:41:44 +0800

    chore: Initial impl of doc site (#213)

---
+++ docs/README.md
@@ -0,0 +1,2 @@
+# Ledger Sync Documentation
+
---
+++ docs/theme.config.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span><b>Ledger Sync</b></span>,
+  project: {
+    link: 'https://github.com/cardano-foundation/cf-ledger-sync',
+  },
+  chat: {
+    link: 'https://discord.gg/jw3yeYJw',
+  },
+  docsRepositoryBase: 'https://github.com/cardano-foundation/cf-ledger-sync',
+  footer: {
+    text: 'Ledger Sync project',
+  },
+   useNextSeoProps() {
+        return {
+            titleTemplate: '%s – Ledger Sync'
+        }
+   },
+    head: (
+        <>
+            <meta property="description" content="Ledger Sync is a Java-based application designed to provide efficient access to Cardano blockchain data"/>
+            <meta property="og:title" content="Ledger Sync is a Java-based application designed to provide efficient access to Cardano blockchain data"/>
+            <meta property="og:description" content="Ledger Sync is a Java-based application designed to provide efficient access to Cardano blockchain data"/>
+        </>
+    )
+}
+
+export default config
