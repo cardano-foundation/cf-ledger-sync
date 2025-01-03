d7722fc8a4c56c127a964764546acafce9857b8c
Sotatek-QuanLeA <quan.lea@sotatek.com>
2025-01-03 18:00:44 +0700

    remove file redundant

--- components/govoffchain-scheduler/src/main/java/org/cardanofoundation/ledgersync/govoffchainscheduler/projection/PoolHashUrlProjection.java
+++
@@ -1,9 +0,0 @@
-// package org.cardanofoundation.ledgersync.scheduler.projection;
-
-// public interface PoolHashUrlProjection {
-//   Long getPoolId();
-
-//   String getUrl();
-
-//   Long getMetadataId();
-// }
